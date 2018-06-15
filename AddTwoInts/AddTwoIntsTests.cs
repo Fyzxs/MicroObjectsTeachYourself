67f288b146275bb77af429e0990a580263f2b6d6
Fyzxs <Fyzxs@users.noreply.github.com>
2018-06-15 09:31:30 -0700

    Initial Implementation w/passing test
    We're taking the simple approach of gettign functionality in place. The test drives the implementation. We're not demonstrating TDD by the book.
    
    With this, we can now start to refactor and microObjectify the code.

--- AddTwoInts/AddTwoIntsTests.cs
+++ AddTwoInts/AddTwoIntsTests.cs
@@ -5,5 +5,24 @@ namespace AddTwoInts
     [TestClass]
     public class AddTwoIntsTests
     {
+        [TestMethod, TestCategory("unit")]
+        public void ShouldReturnSumOfTwoInts()
+        {
+            //Arrange
+            int expected = 9;
+            int augend = 4;
+            int addend = 5;
+
+            //Act
+            int actual = Sum(augend, addend);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        private int Sum(int augend, int addend)
+        {
+            return augend + addend;
+        }
     }
 }
