d67a70924db27c817e1f22fd568b61732ee16f7d
Fyzxs <Fyzxs@users.noreply.github.com>
2018-06-20 16:16:53 -0700

    Wrap Console Write
    Part of the process is to create a class that encapsultes the object that needs the raw data.
    In this case, we're using Console, so we create a class wrapping the console write functionality. It takes an Integer and will write that out.

--- OutputData/OutputDataTests.cs
+++ OutputData/OutputDataTests.cs
@@ -10,7 +10,17 @@ namespace OutputData
         [TestMethod]
         public void ShouldOutputFromInt()
         {
-            Console.WriteLine(new IntegerOf(3));
+            new ConsoleWrite().Into(new IntegerOf(3));
         }
+    }
+
+    public class ConsoleWrite : IWrite
+    {
+        public void Into(Integer item) => Console.WriteLine(item);
+    }
+
+    public interface IWrite
+    {
+        void Into(Integer item);
     }
 }
