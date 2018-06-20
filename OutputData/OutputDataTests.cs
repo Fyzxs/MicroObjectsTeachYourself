6b10e3c15649bc5e9259f6b8ab90ae159313aee6
Fyzxs <Fyzxs@users.noreply.github.com>
2018-06-20 16:35:34 -0700

    Testing the Integer.Into method
    The first test was a visual insepect of the test output window to ensure that the value
    was written properly. This test has us passing in a fake that we can inspect the value of
    to make sure that what we expected to write was actually written. As this is a method
    on the abstract class, all Integers get it for free.

--- OutputData/OutputDataTests.cs
+++ OutputData/OutputDataTests.cs
@@ -14,5 +14,24 @@ namespace OutputData
             Integer integer = new IntegerOf(3);
             integer.Into(writer);
         }
+
+        [TestMethod]
+        public void ShouldTestIntegerWrites()
+        {
+            FakeWrite fakeWrite = new FakeWrite();
+            Integer integer = new IntegerOf(3);
+            integer.Into(fakeWrite);
+
+            Assert.AreEqual((int)integer, fakeWrite.Item);
+        }
+
+        private class FakeWrite : IWrite
+        {
+            public Integer Item;
+            public void Write(Integer item)
+            {
+                Item = item;
+            }
+        }
     }
 }
